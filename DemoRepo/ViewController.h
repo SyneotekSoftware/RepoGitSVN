17415bc18c9b21989f96802505775cee43e4840e
vnanawre <vishal@syneoteksoft.com>
2016-02-17 15:34:32 +0530

    New Import

---
+++ DemoRepo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoRepo
+//
+//  Created by Bhimashankar Vibhute on 17/02/16.
+//  Copyright © 2016 Syneotek Software Solution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoRepo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DemoRepo
+//
+//  Created by Bhimashankar Vibhute on 17/02/16.
+//  Copyright © 2016 Syneotek Software Solution. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
